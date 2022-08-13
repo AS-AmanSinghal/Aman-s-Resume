fc0b53730703bc5198b178d45a3c5fecccaccd77
Aman Singhal <amansinghal.it1@gmail.com>
2022-08-14 01:55:55 +0530

    Test

--- Resume/settings.py
+++ Resume/settings.py
@@ -28,9 +28,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://aman-resume-2.herokuapp.com/']
+ALLOWED_HOSTS = ['aman-resume-2.herokuapp.com']
 
 
 # Application definition
