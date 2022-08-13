a268150d5c95221bfae72ea42257896d533442df
Aman Singhal <amansinghal.it1@gmail.com>
2022-08-14 02:07:35 +0530

    Test

--- Resume/settings.py
+++ Resume/settings.py
@@ -131,7 +131,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-django_heroku.settings(locals(),staticfiles=False)
+django_heroku.settings(locals(),staticfiles=True)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
